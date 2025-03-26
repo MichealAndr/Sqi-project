2ba67d9c92ca22867cdcb5c9fffc61aac612f3d8
Micheal_code <micheal208126@gmail.com>
2025-03-26 14:02:08 +0100

    my commit10

--- otherfiles/cart.js
+++ otherfiles/cart.js
@@ -1,10 +1,10 @@
-let carticon = document.getElementById("carticon");
+const carticon = document.getElementById("carticon");
 const cart = document.getElementById('cart-box');
 const cartClose = document.getElementById('cart-close');
 const order = document.getElementById('order');
 const cartIcon2 = document.getElementById('cartIcon2');
 const cartTotalDisplay = document.getElementById('cartDisplay');
-let CartQualtity = document.getElementById('cartQuatity');
+const CartQualtity = document.getElementById('cartQuatity');
 
 console.log(cartClose, carticon, cartClose, cartIcon2, order);
 
@@ -15,13 +15,27 @@ order.addEventListener("click", () => cart.classList.add('active'));
 cartClose.addEventListener("click", () => cart.classList.remove('active'));
 
 
+
+
+
+
 const checkout = document.getElementById('checkOut');
+console.log(checkout);
+
+const Home = document.getElementById('Home');
 console.log(checkout);
 
 checkout.addEventListener('click', () =>{
      // Redirect to another page after successful login
      setTimeout(() => {
         window.location.href = "../otherfiles/checkout.html"; // Change to your desired URL
+      }, 2000); 
+});
+
+  Home.addEventListener('click', () =>{
+     // Redirect to another page after successful login
+     setTimeout(() => {
+        window.location.href = "./landing.html"; // Change to your desired URL
       }, 2000); 
 });
 
